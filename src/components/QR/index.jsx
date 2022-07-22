a7642caa965fd5e374263bffd5a7c6ff7873a48a
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-22 17:39:41 -0500

    Change qr tag to  prop tag

--- src/components/QR/index.jsx
+++ src/components/QR/index.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Qr } from "../../Models/qr.class";
 import "./style.css";
 
-const QR = ({ title, link, primaryColor, secondaryColor }) => {
+const QR = ({ title, link, primaryColor, secondaryColor, tag }) => {
   const QR = new Qr(title, link, primaryColor, secondaryColor);
   QR.generateQR();
+  const CustomTag = tag ? `${tag}` : "div";
   return (
-    <li className="QR_card" style={{ background: primaryColor }}>
+    <CustomTag className="QR_card" style={{ background: primaryColor }}>
       <div className="QR_imgDecorator">
         <img src={QR.img.src} alt={`QR of ${title}`} className="QR_card_img" />
       </div>
       <h4 className="QR_card_title">{QR.title}</h4>
       <span>➡️ SHARE</span>
-    </li>
+    </CustomTag>
   );
 };
 
