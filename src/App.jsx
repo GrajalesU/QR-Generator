fac2c93aa3eea6d62a20b7aae730fe9e0fd6feb9
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-25 16:02:10 -0500

    Add qr form styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Container from "./components/Container";
+import QRForm from "./components/QRForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Container />
+      {/* <Container /> */}
+      <QRForm />
       <Footer />
     </div>
   );
