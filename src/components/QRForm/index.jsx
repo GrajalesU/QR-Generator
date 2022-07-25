d1d5cb4f0d3f2d05d0ddd5508893f8e8cf6d84a3
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-25 16:01:58 -0500

    update qr from classnames

--- src/components/QRForm/index.jsx
+++ src/components/QRForm/index.jsx
@@ -1,25 +1,36 @@
 import React from "react";
+import "./styles.css";
 
 const QRForm = () => {
   return (
     <div className="qr_form">
       <h2>Create a new QR</h2>
       <form>
-        <div className="form_qr_link">
+        <div className="qr_form_link">
           <label htmlFor="Link">Link</label>
           <input id="Link" type="text" />
+          <small>Example: https://grajalesu.github.io/portfolio/</small>
         </div>
-        <div className="form_qr_title">
-          <label htmlFor="Title">Title</label>
-          <input type="text" id="Title" />
-          <label htmlFor="Title Color">Title Color</label>
-          <input id="Title Color" type="color" />
+        <div className="qr_form_title">
+          <div className="qr_form_title_text">
+            <label htmlFor="Title">Title</label>
+            <input type="text" id="Title" />
+            <small>Example: Portfolio</small>
+          </div>
+          <div className="qr_form_title_color">
+            <label htmlFor="Title Color">Color</label>
+            <input id="Title Color" type="color" />
+          </div>
         </div>
-        <div className="form_qr_colors">
-          <label htmlFor="Primary Color">Primary Color</label>
-          <input id="Primary Color" type="color" />
-          <label htmlFor="Secondary Color">Secondary Color</label>
-          <input id="Secondary Color" type="color" />
+        <div className="qr_form_colors">
+          <div className="qr_form_colors_section">
+            <label htmlFor="Primary Color">Primary Color</label>
+            <input id="Primary Color" type="color" />
+          </div>
+          <div className="qr_form_colors_section">
+            <label htmlFor="Secondary Color">Secondary Color</label>
+            <input id="Secondary Color" type="color" />
+          </div>
         </div>
         <button>Create</button>
       </form>
