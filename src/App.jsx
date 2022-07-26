dc57a4f9a77e22e7ffb51b7a7917b46fe27f10b8
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-26 18:58:44 -0500

    Add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import QRForm from "./components/QRForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Container /> */}
-      {/* <QRForm /> */}
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Container />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/add" element={<QRForm />} />
+          <Route path="*" element={<Container />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
