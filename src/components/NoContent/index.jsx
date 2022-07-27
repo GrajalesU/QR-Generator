fcda1dc78bdbb0d3cb51f772b85ab02f467e3e8a
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-26 22:06:47 -0500

    Add NoContent component html structure

--- src/components/NoContent/index.jsx
+++ src/components/NoContent/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 const NoContent = () => {
   return (
-    <div>You still do not have any QR stored, come on, enter here and encourage yourself to create one to store it in the wallet</div>
-  )
-}
+    <div className="noContent">
+      <strong className="noContent_text">
+        You still do not have any QR stored, come on, enter{" "}
+        <Link to="/add" className="noContent_link">
+          HERE{" "}
+        </Link>
+        and encourage yourself to create one to store it in the wallet
+      </strong>
+    </div>
+  );
+};
 
-export default NoContent
+export default NoContent;
