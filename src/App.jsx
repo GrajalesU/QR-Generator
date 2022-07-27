f5e01a1b705f21f1bcffc5fd0db7c52e7720c6d8
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-26 19:03:23 -0500

    Fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Container />} />
           <Route path="/login" element={<Login />} />
           <Route path="/add" element={<QRForm />} />
           <Route path="*" element={<Container />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
